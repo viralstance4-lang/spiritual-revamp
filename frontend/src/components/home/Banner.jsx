967b282f5d37d58809ebe0f129a01f579233bbab
viralstance4-lang <viralstance4@gmail.com>
2026-05-06 11:46:48 +0530

    vercel file update

--- frontend/src/components/home/Banner.jsx
+++ frontend/src/components/home/Banner.jsx
@@ -1,7 +1,7 @@
 const Banner = ({ desktopImage, mobileImage, alt = "banner", link = "/" }) => {
   return (
     <div className="w-full">
-      <a href={"https://spiritual-revamp.vercel.app/collections"}>
+      <a href={"https://spiritualrevamp.com/collections"}>
         <picture>
           <source media="(max-width: 768px)" srcSet={"https://res.cloudinary.com/df9ftwtis/image/upload/v1776689258/spiritual-revamp/media/ljfuidnpcmtvg7smjc1q.png"} />
           <img

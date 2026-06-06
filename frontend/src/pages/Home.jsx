50adc728f83c5ba39d6258cfda49fde26414ae68
viralstance4-lang <viralstance4@gmail.com>
2026-06-06 18:19:43 +0530

    insta section hide

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -20,7 +20,7 @@ export default function Home() {
       <ProductHighlights />
       <StorySection />
       <Testimonials />
-      <InstagramFeed />
+      {/* <InstagramFeed /> */}
     </>
   );
 }
